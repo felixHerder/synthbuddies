6743daf12f4edd64947954738fc751cb35b82b2c
felixHerder <felixbaciu@gmail.com>
2021-08-16 11:39:29 +0300

    redux connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,54 @@
 import React from "react";
+import { connect } from 'react-redux';
 import Cardlist from '../components/Cardlist';
 import Searchbox from '../components/Searchbox';
-import Scroll from'../components/Scroll';
-import ErroBoundry  from '../components/ErrorBoundry';
+import Scroll from '../components/Scroll';
+import ErroBoundry from '../components/ErrorBoundry';
 import "./App.css";
 
+import { setSearchField } from '../actions';
 
-
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state =  {  
-            robots: [],
-            searchfield: ''
-        }
+const mapStatetoProps = state => {
+    return {
+        searchField: state.searchField
     }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-				.then(response => response.json())
-				.then(users =>this.setState({robots:users}));
-    }
-
-    OnSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
-
-    render(){
-        const frobots = this.state.robots.filter((elem)=> elem.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-				
-			return !this.state.robots.length ?
-				<h1>Loading synths...</h1>
-				:
-				(<div className="tc">
-					<h1  className='f2'>Synthfriends</h1>
-					<Searchbox searchChange={this.OnSearchChange}/>
-					<Scroll>
-                        <ErroBoundry>
-						    <Cardlist robots={frobots}/>
-                        </ErroBoundry>
-					</Scroll>
-				</div>);
-				
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
     }
 }
 
-export default App;
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    render() {
+        const frobots = this.state.robots.filter((elem) => elem.name.toLowerCase().includes(this.props.searchField.toLowerCase()));
+
+        return !this.state.robots.length ?
+            <h1>Loading synths...</h1>
+            :
+            (<div className="tc">
+                <h1 className='f2'>Synthfriends</h1>
+                <Searchbox searchChange={this.props.onSearchChange} />
+                <Scroll>
+                    <ErroBoundry>
+                        <Cardlist robots={frobots} />
+                    </ErroBoundry>
+                </Scroll>
+            </div>);
+    }
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
